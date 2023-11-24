a8244b0e3975a49ffe0f115f09ed664051c63f58
icantkode <ashersiddique127@gmail.com>
2023-11-25 03:05:42 +0500

    base setup

---
+++ src/main/java/com/asher/scd_pixelquestweb/ScdPixelquestwebApplication.java
@@ -0,0 +1,13 @@
+package com.asher.scd_pixelquestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdPixelquestwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScdPixelquestwebApplication.class, args);
+    }
+
+}
